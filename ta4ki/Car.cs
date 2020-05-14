55d306eb4f60ef4d38e3db17f6d6ef4dc40d995c
yaremakadanil@gmail.com <yaremakadanil@gmail.com>
2020-05-14 15:29:07 +0700

    objects

---
+++ ta4ki/Car.cs
@@ -0,0 +1,52 @@
+﻿using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ta4ki
+{
+    public class Car : MapObject
+    {
+        private PointLatLng point;
+
+        public Car(string title, PointLatLng point) : base(title)
+        {
+            this.point = point;
+        }
+
+        public override double getDistance(PointLatLng point)
+        {
+            GeoCoordinate p1 = new GeoCoordinate(point.Lat, point.Lng);
+            GeoCoordinate p2 = new GeoCoordinate(this.point.Lat, this.point.Lng);
+
+            return p1.GetDistanceTo(p2);
+        }
+
+        public override PointLatLng getFocus()
+        {
+            return point;
+        }
+
+        public override GMapMarker getMarker()
+        {
+            GMapMarker marker = new GMapMarker(point)
+            {
+                Shape = new Image
+                {
+                    Width = 32, // ширина маркера
+                    Height = 32, // высота маркера
+                    ToolTip = this.getTitle(), // всплывающая подсказка
+                    Source = new BitmapImage(new Uri("pack://application:,,,/pictures/mater.png")) // картинка
+                }
+            };
+
+            return marker;
+        }
+    }
+}
